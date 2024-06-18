8c7cfd3a9391b680b865168643536061dfb68a52
Guilherme <guilhermeshadow91@gmail.com>
2024-06-18 14:24:33 -0300

    refactor: improving types

--- src/repositories/bill/bill.repository.ts
+++ src/repositories/bill/bill.repository.ts
@@ -1,35 +1,37 @@
-import { Bill, bill_types } from '@prisma/client';
-import { Dayjs } from 'dayjs';
+import { Bill } from '@prisma/client';
 import { UserFromJwt } from 'src/auth/models/UserFromJwt';
 import {
   BillDto,
   CreateBillDto,
   FilterBill,
+  FilterBillInterval,
   UpdateBillDto,
+  UpdateBillNotifyParams,
+  UpdateBillStatus,
 } from 'src/bill/dto/bill.dto';
 
 export abstract class BillRepository {
   abstract createBill(dto: CreateBillDto, user: UserFromJwt): Promise<Bill>;
+
   abstract getBills(filters: FilterBill, user: UserFromJwt): Promise<BillDto[]>;
+
   abstract getAllBills(): Promise<Bill[]>;
-  abstract getBillsDueTomorrow(date: Dayjs): Promise<Bill[]>;
-  abstract getBillsDueToday(date: Dayjs): Promise<Bill[]>;
+
+  abstract getBillsDueTomorrow(params: FilterBillInterval): Promise<Bill[]>;
+
+  abstract getBillsDueToday(params: FilterBillInterval): Promise<Bill[]>;
+
   abstract getOneBill(id: number, user: UserFromJwt): Promise<BillDto>;
+
   abstract updateBill(
     id: number,
     dto: UpdateBillDto,
     user: UserFromJwt,
   ): Promise<Bill>;
+
   abstract deleteBill(id: number, user: UserFromJwt): Promise<void>;
-  abstract updateBillStatus(
-    id: number,
-    status: bill_types,
-    now: Dayjs,
-  ): Promise<void>;
-  abstract updateBillNotify(
-    id: number,
-    email: string,
-    type: string,
-    now: Dayjs,
-  ): Promise<void>;
+
+  abstract updateBillStatus(params: UpdateBillStatus): Promise<void>;
+
+  abstract updateBillNotify(params: UpdateBillNotifyParams): Promise<void>;
 }
