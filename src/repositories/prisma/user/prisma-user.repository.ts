ba9f6e506e6dec7dda66246184b7fbfda3918272
Guilherme <guilhermeshadow91@gmail.com>
2024-07-01 18:57:49 -0300

    refactor(import): removing useless code

--- src/repositories/prisma/user/prisma-user.repository.ts
+++ src/repositories/prisma/user/prisma-user.repository.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { UserRepository } from '../../user/user.repository';
 import {
