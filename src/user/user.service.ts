6792b437b8d16efce77f807df710fdfc0066ba6d
Guilherme <guilhermeshadow91@gmail.com>
2024-08-22 18:38:59 -0300

    feat(user.service): adding baseUrl variable and make code more clean with new methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,32 +9,21 @@ import { UserInfos, CreateUserDto, UpdateUserDto } from './dto/user.dto';
 import { RecordWithId } from '../../src/common/record-with-id.dto';
 import { User } from '@prisma/client';
 import { MailerService } from '@nestjs-modules/mailer';
-import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
+  private readonly baseUrl: string =
+    this.configService.get('BASE_URL') ?? 'http://localhost:3005';
+
   constructor(
     private readonly userRepository: PrismaUserRepository,
+    private readonly configService: ConfigService,
     @Inject(MailerService) private readonly mailerService: MailerService,
   ) {}
 
-  //Isso aqui tem que mudar, acho sem nexo ficar aqui mas depois eu olho isso.
-  private urlGen(request: Request): string {
-    const protocol = request.protocol;
-    const host = request.get('Host');
-    const originalUrl = request.originalUrl;
-    const fullUrl = `${protocol}://${host}${originalUrl}`;
-
-    return fullUrl;
-  }
-
-  private sendEmailValidationLink(
-    email: string,
-    code: string,
-    request: Request,
-  ) {
-    const url = this.urlGen(request);
-
+  private sendEmailValidationLink(email: string, code: string) {
+    const validationUrl = `${this.baseUrl}/user/validate-account/${code}`;
     return this.mailerService.sendMail({
       to: email,
       subject: 'Welcome to our platform',
@@ -42,41 +31,20 @@ export class UserService {
       html: `
       <div>
       <h1>You have successfully created an account</h1>
-      <p>To access our platform and enjoy all the features, you have to validate your account first in the link: ${
-        url + '/validate-account/' + code
-      }</p>
+      <p>To access our platform and enjoy all the features, you have to validate your account first using the following link: 
+      <a href="${validationUrl}">${validationUrl}</a></p>
       </div>`,
     });
   }
 
-  //Esse método também não é a melhor coisa do mundo.
-  //Eu não gostaria de implementar filas por agora, mas acho que é o melhor a se fazer pro futuro.
-  //Pois assim eu chamo o método de envio de email e ele(atualmente) nem tem como saber se vai dar errado.
-  //Ao menos, o pior(eu acho) dos casos, o email não vai chegar...
-  //A parte boa é que eu garanto o rápido retorno sem esperar a execução do envio do email.
-  async createUser(
-    dto: CreateUserDto,
-    request: Request,
-  ): Promise<RecordWithId> {
+  async createUser(dto: CreateUserDto): Promise<RecordWithId> {
     const user = await this.userRepository.findUserByEmail(dto.email);
 
-    //Preciso melhorar essa lógica aqui.
-    //Ainda não finalizado.
     if (user && !user.is_active) {
-      const now = new Date();
-      const codeVerification = await this.userRepository.findCodeVerification(
-        user.id,
+      await this.handleInactiveUser(user);
+      throw new BadRequestException(
+        'User already exists but still inactive. Please check your email, we sent a new validation link!',
       );
-
-      const expireDate = new Date(codeVerification.expire_date);
-
-      if (now < expireDate) {
-        this.sendEmailValidationLink(dto.email, codeVerification.code, request);
-      }
-
-      return {
-        id: user.id,
-      };
     }
 
     if (user && user.is_active) {
@@ -85,25 +53,59 @@ export class UserService {
 
     const createdUser = await this.userRepository.createUser(dto);
 
-    const codeVerification = await this.userRepository.findCodeVerification(
-      createdUser.id,
-    );
-
-    this.sendEmailValidationLink(dto.email, codeVerification.code, request);
+    await this.handleNewUser(createdUser);
 
     return {
       id: createdUser.id,
     };
   }
 
-  //Esse método vai se extender.
-  //Continuar isso aqui, não esquecer
-  async validateAccount(id: number): Promise<void> {
-    const user = await this.userRepository.findUserComplete(id);
+  private async handleInactiveUser(user: User) {
+    const now = new Date();
+    const codeVerification = await this.userRepository.findCodeVerification(
+      user.id,
+    );
+
+    const expireDate = new Date(codeVerification.expire_date);
+
+    if (now < expireDate) {
+      this.sendEmailValidationLink(user.email, codeVerification.code);
+      return;
+    }
+
+    const newCodeVerification =
+      await this.userRepository.handleCodeVerification(
+        user,
+        codeVerification,
+        now,
+      );
+
+    this.sendEmailValidationLink(user.email, newCodeVerification.code);
+  }
+
+  private async handleNewUser(user: User) {
+    const codeVerification = await this.userRepository.findCodeVerification(
+      user.id,
+    );
+
+    this.sendEmailValidationLink(user.email, codeVerification.code);
+  }
+
+  async validateAccount(code: string): Promise<void> {
+    const user = await this.userRepository.findUserByCode(code);
 
     if (!user) throw new NotFoundException('User not found');
 
-    return await this.userRepository.validateAccount(user);
+    const codeVerification = await this.userRepository.validateAccount(user);
+
+    if (codeVerification) {
+      this.sendEmailValidationLink(user.email, codeVerification.code);
+      throw new BadRequestException(
+        'Code already expired, we sent a new one to your email',
+      );
+    }
+
+    return;
   }
 
   async findUserByEmail(email: string): Promise<User> {
