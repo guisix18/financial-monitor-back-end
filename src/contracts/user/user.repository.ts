47c7228890e55099ffd4dba8dd8e19dd88d11985
Guilherme <guilhermeshadow91@gmail.com>
2024-08-22 00:21:33 -0300

    feat(contracts): adding more abstract methods to be implemented

--- src/contracts/user/user.repository.ts
+++ src/contracts/user/user.repository.ts
@@ -1,4 +1,4 @@
-import { User } from '@prisma/client';
+import { CodeVerification, User } from '@prisma/client';
 import { UserInfos, CreateUserDto, UpdateUserDto } from 'src/user/dto/user.dto';
 
 export abstract class UserRepository {
@@ -8,4 +8,9 @@ export abstract class UserRepository {
   abstract updateUser(dto: UpdateUserDto, id: number): Promise<User>;
   abstract deleteUser(id: number): Promise<void>;
   abstract validateAccount(user: User): Promise<void>;
+  abstract findCodeVerification(user_id: number): Promise<CodeVerification>;
+  abstract createCodeVerification(
+    user_id: number,
+    now: Date,
+  ): Promise<CodeVerification>;
 }
