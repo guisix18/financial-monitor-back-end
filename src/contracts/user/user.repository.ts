fc4d9aa8f8ed323c4c26018361fd2979c86f8256
Guilherme <guilhermeshadow91@gmail.com>
2024-08-22 18:36:18 -0300

    feat(contracts): adding more contracts to hold code verification functions in user repository

--- src/contracts/user/user.repository.ts
+++ src/contracts/user/user.repository.ts
@@ -1,16 +1,39 @@
-import { CodeVerification, User } from '@prisma/client';
+import { CodeVerification, Prisma, User } from '@prisma/client';
+import { UserWithCodeVerification } from 'src/repositories/prisma/user/prisma-user.repository';
 import { UserInfos, CreateUserDto, UpdateUserDto } from 'src/user/dto/user.dto';
 
 export abstract class UserRepository {
   abstract createUser(dto: CreateUserDto): Promise<User>;
+
   abstract findUserByEmail(email: string): Promise<User>;
+
   abstract findOneUser(id: number): Promise<UserInfos>;
+
   abstract updateUser(dto: UpdateUserDto, id: number): Promise<User>;
+
   abstract deleteUser(id: number): Promise<void>;
-  abstract validateAccount(user: User): Promise<void>;
+
+  abstract validateAccount(user: User): Promise<CodeVerification | null>;
+
   abstract findCodeVerification(user_id: number): Promise<CodeVerification>;
+
   abstract createCodeVerification(
     user_id: number,
+    now: Date,
+    prismaTx: Prisma.TransactionClient,
+  ): Promise<CodeVerification>;
+
+  abstract findUserByCode(code: string): Promise<UserWithCodeVerification>;
+
+  abstract invalidateCodeVerification(
+    user_id: number,
+    code_id: number,
+    prismaTx: Prisma.TransactionClient,
+  ): Promise<void>;
+
+  abstract handleCodeVerification(
+    user: User,
+    codeVerification: CodeVerification,
     now: Date,
   ): Promise<CodeVerification>;
 }
